dcda24e86777f4768d3af937f0f5dfe95f9941f3
yuu ogasawara <performa532@gmail.com>
2018-06-08 15:32:06 +0900

    remove unnecessary blank row

--- ios-SKStoreReviewController-sample/ios-SKStoreReviewController-sample/ViewController.swift
+++ ios-SKStoreReviewController-sample/ios-SKStoreReviewController-sample/ViewController.swift
@@ -15,14 +15,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        
         showReviewAlert()
     }
     // MARK: SKStoreReviewController
@@ -48,18 +46,14 @@ class ViewController: UIViewController {
                                       message: "いつもありがとうございます！\nレビューをお願いします！",
                                       preferredStyle: .alert)
         self.present(alert, animated: true, completion: nil)
-        
         let cancelAction = UIAlertAction(title: "キャンセル",
                                          style: .cancel,
                                          handler: nil)
         alert.addAction(cancelAction)
-        
         let reviewAction = UIAlertAction(title: "レビューする",
                                          style: .default,
                                          handler: {
                                             (action:UIAlertAction!) -> Void in
-                                            
-                                            
                                             if #available(iOS 10.0, *) {
                                                 UIApplication.shared.open(url, options: [:])
                                             }
